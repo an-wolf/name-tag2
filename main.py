c7f4fcb5234a0487d7772f49a114040e0bbdf3cb
an-wolf <110436808+an-wolf@users.noreply.github.com>
2022-11-23 19:16:56 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+def on_forever():
+   basic.show_string("Hi, my nickname is: ")
+   basic.show_string(" An Wolf16!")
+   basic.show_string(" I like programming ")
+   basic.show_number( 0.0 )
+   basic.show_string(" bye... ")
+
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function on_forever() {
+    basic.showString("Hi, my nickname is: ")
+    basic.showString(" An Wolf16!")
+    basic.showString(" I like programming ")
+    basic.showNumber(0.0)
+    basic.showString(" bye... ")
+})
